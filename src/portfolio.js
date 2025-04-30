2e62d006b3dac2ccd4136fe1ef8c9615c35c878a
Utkarsh <utkarsh7314@gmail.com>
2025-04-30 18:32:33 +0100

    removed grdive link

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,8 +25,7 @@ const greeting = {
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
-  resumeLink:
-    "https://drive.google.com/file/d/1T9Do5BolIcHGJzSf3nNAd5tP8mJ4KmQ-/view?usp=sharing", // Set to empty to hide the button
+  resumeLink: "", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
